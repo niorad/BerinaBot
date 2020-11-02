05f2e279f9b65134f8973c0f1bf244eb36612925
Antonio Radovcic <mail@niorad.com>
2020-11-02 18:09:25 +0100

    Stuff

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const config = require('./config.json');
 const Discord = require("discord.js");
 
 
-async function getCurrentProducts() {
+function generateProductLists(products) {
+   return products.map((list) => {
+      return list.map((item) => {
+         return `${item.querySelector("[itemprop=name]").content}§§§${config.BASE_URL}${item.querySelector(".product-link").href}`;
+      });
+   });
+}
 
+async function getCurrentProducts() {
    let products = [];
 
    for(let i = 0; i < config.DATA_SOURCE_URLS.length; i++) {
@@ -17,14 +24,8 @@ async function getCurrentProducts() {
       products.push(Array.from(dom.window.document.querySelectorAll(".product-tile")));
    }
 
-
-   console.log(products.flat());
-
-   return products.flat().map((item) => {
-      return `${item.querySelector("[itemprop=name]").content}§§§${config.BASE_URL}${item.querySelector(".product-link").href}`;
-   });
+   return generateProductLists(products);
 }
-
 
 async function getLastProductList() {
    if(fs.existsSync('./_results.json')) {
@@ -39,8 +40,15 @@ async function getLastProductList() {
 
 // Get all new Items from current that are not in lastSaved
 function getNewProducts(current, lastSaved) {
-   return current.reduce((acc, cur, index) => {
-      if(lastSaved.indexOf(cur) === -1 && index < 5) {
+
+   //trim to 6 per list
+   const trimmedProductLists = current.map((list) => list.slice(0,6));
+   const flattenedProducts = trimmedProductLists.flat(1);
+   const dedupedCurrentProducts = [...new Set(flattenedProducts)];
+   const dedupedLastProducts = [...new Set(lastSaved)];
+
+   return dedupedCurrentProducts.reduce((acc, cur, index) => {
+      if(dedupedLastProducts.indexOf(cur) === -1) {
          return [...acc, cur];
       } else {
          return acc;
@@ -48,35 +56,34 @@ function getNewProducts(current, lastSaved) {
    }, []);
 }
 
-function postNewProductsToDiscord(products) {
+
+async function postNewProductsToDiscord(products) {
 
    const client = new Discord.Client();
    client.login(config.BOT_TOKEN);
 
-   const list = products.reduce((acc, val) => {
-      // const title = val.split('§§§')[0];
-      const link = val.split('§§§')[1];
-      return `${acc}\n ${link}`;
-   }, '');
+   client.on("ready", async () => {
+      await client.channels.cache.get(config.CHANNEL_ID).send(`${config.GREETING}`);
 
-   client.on("ready", () => {
-      client.channels.cache.get(config.CHANNEL_ID).send(`${config.GREETING}${list}`).then(() => {
-         client.destroy();
-         return process.exit(1);
-      });
+      for(let i = 0; i < products.length; i++) {
+         await client.channels.cache.get(config.CHANNEL_ID).send(products[i].split('§§§')[1])
+      }
+
+      client.destroy();
+      return process.exit(1);
    });
 }
 
 async function storeNewProductList(products) {
-   fs.writeFileSync('./_results.json', JSON.stringify(products));
+   fs.writeFileSync('./_results.json', JSON.stringify(products.flat()));
 }
 
 getCurrentProducts().then(currentProducts => {
-   const dedupedCurrentProducts = [...new Set(currentProducts)];
    getLastProductList().then(lastProductList => {
-      storeNewProductList(dedupedCurrentProducts).then(() => {
-         const newProducts = getNewProducts(dedupedCurrentProducts, lastProductList);
+      storeNewProductList(currentProducts).then(() => {
+         const newProducts = getNewProducts(currentProducts, lastProductList);
          if(newProducts.length > 0) {
+            console.log(newProducts);
             postNewProductsToDiscord(newProducts);
          }
       });
--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-const client = new Discord.Client();
-client.login(config.BOT_TOKEN);
-
-  client.on("ready", () => {
-    // client.channels.cache.get(config.CHANNEL_ID).send(JSON.stringify(names));
-    console.log("Posting to chat: ", JSON.stringify(names));
-  });
